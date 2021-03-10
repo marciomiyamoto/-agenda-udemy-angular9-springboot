7d4480a659affa34870628b726902e8e19c7b8f2
marciomiyamoto <marcio_takashi@hotmail.com>
2021-03-09 23:33:21 -0300

    Implementando formulario para adicionar novo contato

--- agenda-app/src/app/app.module.ts
+++ agenda-app/src/app/app.module.ts
@@ -3,18 +3,40 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatButtonModule} from '@angular/material/button';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar'
+import { MatInputModule } from '@angular/material/input';
+import { ContatoComponent } from './contato/contato.component';
+import { ContatoService } from './contato.service';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatTableModule } from '@angular/material/table';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContatoComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatButtonModule
+    MatButtonModule,
+    MatToolbarModule,
+    MatInputModule, 
+    HttpClientModule,
+    ReactiveFormsModule,
+    MatTabsModule,
+    MatTableModule,
+    MatIconModule,
+    MatCardModule
   ],
-  providers: [],
+  providers: [
+    ContatoService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ agenda-app/src/app/contato/contato.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { ContatoService } from '../contato.service';
+import { Contato } from './contato';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-contato',
+  templateUrl: './contato.component.html',
+  styleUrls: ['./contato.component.css']
+})
+export class ContatoComponent implements OnInit {
+
+  formulario: FormGroup;
+  contatos: Contato[] = [];
+
+  constructor(
+    private service: ContatoService,
+    private fb: FormBuilder
+  ) { }
+
+  ngOnInit(): void { 
+    this.formulario = this.fb.group({
+      nome: ['', Validators.required],
+      email: ['', [Validators.email, Validators.required]]
+    });
+  }
+
+  submit() {
+    const formValues = this.formulario.value; 
+    const contato: Contato = new Contato(formValues.nome, formValues.email);
+
+    this.service
+    .save(contato)
+    .subscribe( response => {
+      this.contatos.push(response);
+      console.log(this.contatos)
+    });
+  }
+
+}
